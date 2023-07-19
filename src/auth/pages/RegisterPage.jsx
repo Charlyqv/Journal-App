2b81b17b4ff86815a05cab748d84b2b37abd0c21
Carlos <charlyqv98@gmail.com>
2023-07-19 14:57:36 -0600

    AuthLayout

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,7 +1,49 @@
+import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { AuthLayout } from '../layout/AuthLayout';
 
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayout tittle='Register'>
+      <form>
+        <Grid container>
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField 
+              label="Nombre"
+              type="text"
+              placeholder='Nombre'
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField 
+              label="Correo"
+              type="email"
+              placeholder='correo@google.com'
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={ 12 } sx={{ mt: 2 }}>
+            <TextField 
+              label="Contraseña"
+              type="password"
+              placeholder='*******'
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={ 12 } sm={ 6 }>
+              <Button variant='contained' fullWidth>
+                Register
+              </Button>
+            </Grid>
+          </Grid>
+
+        </Grid>
+      </form>
+    </AuthLayout>
   )
 }
