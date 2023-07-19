acc01dfde6cc220bcd8c2b46b35dd68e1e35cf96
Carlos <charlyqv98@gmail.com>
2023-07-19 15:03:31 -0600

    RegisterPage - Diseño

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,17 +1,18 @@
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
 import { AuthLayout } from '../layout/AuthLayout';
 
 
 export const RegisterPage = () => {
   return (
-    <AuthLayout tittle='Register'>
+    <AuthLayout tittle='Crear cuenta'>
       <form>
         <Grid container>
           <Grid item xs={ 12 } sx={{ mt: 2 }}>
             <TextField 
-              label="Nombre"
+              label="Nombre completo"
               type="text"
-              placeholder='Nombre'
+              placeholder='Nombre completo'
               fullWidth
             />
           </Grid>
@@ -35,11 +36,18 @@ export const RegisterPage = () => {
           </Grid>
 
           <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
-            <Grid item xs={ 12 } sm={ 6 }>
+            <Grid item xs={ 12 } sm={ 12 }>
               <Button variant='contained' fullWidth>
-                Register
+                Crear cuenta
               </Button>
             </Grid>
+          </Grid>
+
+          <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{ mr: 1 }}>¿Ya tienes cuenta?</Typography>
+            <Link component={ RouterLink }color='inherit' to="/auth/login">
+              Ingresar
+            </Link>
           </Grid>
 
         </Grid>
