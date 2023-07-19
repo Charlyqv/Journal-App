1ca9d564a4a7036bc7364be58307cc2ada334170
Carlos <charlyqv98@gmail.com>
2023-07-19 16:34:16 -0600

    NothindSelectedView - No hay nada seleccionado

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,14 @@
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
-      <Typography>Elit eu ipsum ex qui adipisicing exercitation magna tempor qui adipisicing excepteur id.</Typography>
+      {/* <Typography>Elit eu ipsum ex qui adipisicing exercitation magna tempor qui adipisicing excepteur id.</Typography> */}
+      <NothingSelectedView />
 
-      {/* NothinSelected */}
       {/* NoteVire */}
     </JournalLayout>
   )
