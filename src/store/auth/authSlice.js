063c43fd570cdd56d820b8da82ceb2b35b46c9c9
Carlos <charlyqv98@gmail.com>
2023-07-31 15:46:14 -0600

    Disparar acción de autenticación

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -11,9 +11,21 @@ export const authSlice = createSlice({
     },
     reducers: {
       login: ( state, action ) => {
+        state.status = 'authenticated'; // 'cheking'; 'not-authenticated'; 'authenticated'
+        state.uid = payload.uid;
+        state.email = payload.email;
+        state.displayName = payload.displayName;
+        state.photoURL = payload.photoURL;
+        state.errorMessage = null;
 
       },
       logout: ( state, payload ) => {
+        state.status = 'not-authenticated'; // 'cheking'; 'not-authenticated'; 'authenticated'
+        state.uid = null;
+        state.email = null;
+        state.displayName = null;
+        state.photoURL = null;
+        state.errorMessage = payload.errorMessage;
 
       },
       checkingCredentials: (state) => {
