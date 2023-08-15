c1a2e28e8a6dacbd21be481fb2e113906d0b2868
Carlos <charlyqv98@gmail.com>
2023-08-15 09:45:23 -0600

    JournalSlice

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,46 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+    name: 'journal',
+    initialState: {
+        isSaving: true,
+        mesaggeSaved: '',
+        notes: [],
+        active: null,
+        // active: {
+        //   id: 'ABC123',
+        //   title: '',
+        //   body: '',
+        //   date: 1234567,
+        //   imageUrls: [],
+        // }
+    },
+    reducers: {
+      addNewEmptyNote: ( state, action ) => {
+
+      },
+      setActiveNote: ( state, action ) => {
+
+      },
+      setNotes: ( state, action ) => {
+
+      },
+      setSaving: ( state ) => {
+
+      },
+      updateNote: ( state, action ) => {
+
+      },
+      deleteNoteById: ( state, action ) => {
+
+      },
+    }
+});
+export const { 
+  addNewEmptyNote,
+  setActiveNote,
+  setNotes,
+  setSaving,
+  updateNote,
+  deleteNoteById, 
+} = journalSlice.actions;
